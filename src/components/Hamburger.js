e298b0f5901247944f200a3abe2cd5c02bda2b6e
pksung1 <pksung1@gmail.com>
2022-01-26 03:14:26 +0900

    feat : Hamburgur animation

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Hamburger = ({onClick}) => {
   return (
-    <svg version="1.1" baseProfile="full" width="35" height="25" onClick={onClick}>
-      <rect width="35" height="2" fill="#000" rx="5" ry="5"/>
-      <rect width="35" height="2" y="10" fill="#000" rx="5" ry="5"/>
-      <rect width="35" height="2" y="20" fill="#000" rx="5" ry="5"/>
+    <svg version="1.1" baseProfile="full" width="35" height="25" onClick={onClick} className='overflow-hidden group'>
+      <rect width="35" height="2" fill="#000" rx="5" ry="5" className='group-hover:translate-x-0 translate-x-2 transition-transform duration-300'/>
+      <rect width="35" height="2" y="10" fill="#000" rx="5" ry="5" className='group-hover:translate-x-0 translate-x-4 transition-transform duration-300'/>
+      <rect width="35" height="2" y="20" fill="#000" rx="5" ry="5" className='group-hover:translate-x-0 translate-x-6 transition-transform duration-300'/>
     </svg>
   )
 }
