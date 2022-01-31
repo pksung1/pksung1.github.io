39dcc00ecb91f9589a0812525f3307268982f7e4
pksung1 <pksung1@gmail.com>
2022-02-01 03:57:06 +0900

    feat : header menu darkmode added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -29,19 +29,22 @@ const Menu = () => {
     setDisplayMenu(!displayMenu)
   }
 
-  const mobileMenuClasses = classNames('flex flex-col items-center absolute left-0 top-12 bg-white w-full shadow', {'hidden': !displayMenu})
+  const mobileMenuClasses = classNames('flex flex-col items-center absolute left-0 top-12 bg-white dark:bg-gray-900 w-full shadow', {'hidden': !displayMenu})
 
   
   return (
     <div className='flex items-center'>
-        <div className="flex flex-row justify-center items-center gap-4 hidden lg:flex">
+        <div className="flex-row justify-center items-center gap-4 hidden lg:flex dark:text-white">
           {MENUS.map(({to, display}) => <Link to={to} key={to}>{display}</Link>)}
           <DarkModeSwitch />
         </div>
+        
         <div className="lg:hidden flex items-center">
           <Hamburger onClick={handleMenuClick} />
           <div className={mobileMenuClasses}>
-            {MENUS.map(({to, display}) => <Link className='py-2 w-full shadow text-center' to={to} key={to}>{display}</Link>)}
+            <DarkModeSwitch />
+            {MENUS.map(({to, display}) => <Link className='py-2 w-full shadow dark:shadow-gray-300 text-center dark:text-white' to={to} key={to}>{display}</Link>)}
+            
           </div>
         </div>
     </div>
